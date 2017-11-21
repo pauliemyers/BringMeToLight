1d78574dd58de427150f632394a05665aca1d24b
Paulie Myers <pam528@nyu.edu>
2017-11-21 09:21:14 -0500

    Edited scripts
    Checking to see how GitHub handles script changes

---
+++ Assets/Scripts/CollisionChanger.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionChanger : MonoBehaviour {
+
+	public Material matGrass;
+	public Material matGrassDark;
+	public Material matMeadow;
+	public Material matSand;
+	public Material matEarth;
+	public Material matHill;
+	public Material matMountain;
+	public Material matSnow;
+	public Material matSky;
+	public Material matStar;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if (col.gameObject.tag == "grass") {
+			var rend = col.gameObject.GetComponent<Renderer> ();
+			rend.material = matGrass;
+		}
+		else if (col.gameObject.tag == "grassDark") {
+			var rend = col.gameObject.GetComponent<Renderer> ();
+			rend.material = matGrassDark;
+		}
+		else if (col.gameObject.tag == "snow") {
+			var rend = col.gameObject.GetComponent<Renderer> ();
+			rend.material = matSnow;
+		}
+		else if (col.gameObject.tag == "mountain") {
+			var rend = col.gameObject.GetComponent<Renderer> ();
+			rend.material = matMountain;
+		}
+		else if (col.gameObject.tag == "hill") {
+			var rend = col.gameObject.GetComponent<Renderer> ();
+			rend.material = matHill;
+		}
+		else if (col.gameObject.tag == "earth") {
+			var rend = col.gameObject.GetComponent<Renderer> ();
+			rend.material = matEarth;
+		}
+		else if (col.gameObject.tag == "meadow") {
+			var rend = col.gameObject.GetComponent<Renderer> ();
+			rend.material = matMeadow;
+		}
+		else if (col.gameObject.tag == "sand") {
+			var rend = col.gameObject.GetComponent<Renderer> ();
+			rend.material = matSand;
+		}
+		else if (col.gameObject.tag == "sky") {
+			var rend = col.gameObject.GetComponent<Renderer> ();
+			rend.material = matSky;
+		}
+		else if (col.gameObject.tag == "star") {
+			var rend = col.gameObject.GetComponent<Renderer> ();
+			rend.material = matStar;
+		}
+	}
+}
--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -5,24 +5,41 @@ using UnityEngine;
 public class PlayerMover : MonoBehaviour {
 	public float speed;
 
+	public Vector3 jump;
+	public float jumpForce = 6.0f;
+	public float jumpHeight = 2.0f;
+
+	private bool isGrounded;
+
+	Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody> ();
+		jump = new Vector3 (0.0f, jumpHeight, 0.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.RightArrow)) {
 			transform.Translate (speed, 0, 0);
-		}
-		else if (Input.GetKey (KeyCode.LeftArrow)) {
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
 			transform.Translate (-speed, 0, 0);
-		}
-		else if (Input.GetKey (KeyCode.UpArrow)) {
+		} else if (Input.GetKey (KeyCode.UpArrow)) {
 			transform.Translate (0, 0, speed);
-		}
-		else if (Input.GetKey (KeyCode.DownArrow)) {
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
 			transform.Translate (0, 0, -speed);
 		}
+
+		if (Input.GetKeyDown (KeyCode.Space) && isGrounded) {
+
+			rb.AddForce (jump * jumpForce, ForceMode.Impulse);
+			isGrounded = false;
+		}
+	}
+
+	void OnCollisionStay() {
+		isGrounded = true;
 	}
 }
+
